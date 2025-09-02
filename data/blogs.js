4af40f0e9ab4bf5d19d208dc2ab086ae502b6282
Hiro <sonamtopgey369@gmail.com>
2025-09-02 21:21:40 +0530

    Made generated html for blogs

---
+++ data/blogs.js
@@ -0,0 +1,24 @@
+export let blogs = [
+    {
+        title: 'First Blog',
+        id: '1',
+        dateCreated: '02.07.25',
+        lastModifiedDate: '03.08.25',
+        lastModifiedTime: '03.08.25',
+        content: 'This is my first every blog.'
+    },
+    {
+        title: 'Second Blog',
+        id: '2',
+        lastModifiedDate: '03.08.25',
+        lastModifiedTime: '03.08.25',
+        content: 'This is my second every blog.'
+    },
+    {
+        title: 'Third Blog',
+        id: '3',
+        lastModifiedDate: '03.08.25',
+        lastModifiedTime: '03.08.25',
+        content: 'This is my third every blog.'
+    },
+]
---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+import { blogs } from "../data/blogs.js";
+
+let blogsHTML = '';
+
+blogs.forEach(blog => {
+
+    let blogCardsContainer = document.querySelector('.blog-cards-container');
+
+    blogsHTML += `
+
+        <div
+            class="blog-card-container"
+        >   
+            <!-- Blog Title Heading Container -->
+            <div
+                class="blog-title-heading-container"
+            >
+
+                <!-- Blog Title Heading -->
+                <h4
+                    class="blog-title-heading"
+                >
+                    ${blog.title}
+                </h4>
+
+            </div>
+
+            <!-- Blog Content Paragraph Container -->
+            <div
+                class="blog-content-paragraph-container"
+            >
+
+                <!-- Blog Content Paragraph -->
+                <p
+                    class="blog-content-paragraph"
+                >
+                    ${blog.content}
+                </p>
+
+            </div>
+
+            <!-- Blog Date Created Paragraph Container -->
+            <div
+                class="blog-date-created-paragraph-container"
+            >
+
+                <!-- Blog Date Created Paragraph -->
+                <p
+                    class="blog-date-created-paragraph"
+                >
+                    Created: ${blog.dateCreated}
+                </p>
+
+            </div>
+
+            <!-- Blog Last Modified Time Container -->
+            <div
+                class="blog-last-modified-time-paragraph-container"
+            >
+
+                <!-- Blog Last Modified Time Paragraph -->
+                <p
+                    class="blog-last-modified-time-paragraph"
+                >
+                Last Modified: ${blog.lastModifiedDate} at ${blog.lastModifiedTime}
+                </p>
+
+            </div>
+
+        </div>
+
+    `
+
+    blogCardsContainer.innerHTML = blogsHTML;
+
+})
